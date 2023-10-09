5e26c47b47da176bc0129b934109b81f04eea4bf
Anvar <anvar.ganiev99@gmail.com>
2023-10-09 15:49:08 -0400

    more fixes

--- server/server.js
+++ server/server.js
@@ -12,6 +12,9 @@ const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  persistedQueries: {
+    cache: 'bounded', // Use a bounded cache for performance
+  },
   context: authMiddleware,
 });
 
